2705e3c5eee44b45c1353d4238b65f024b5d0361
rudolfsonjunior <rudolfson.junior@gmail.com>
2016-05-25 00:52:50 +0200

    ADDED: assets/src/modules/pages/, added all modules for the different pages(views), standart: error,footer,header,home(logged user),landing(non logged user),profile

---
+++ assets/src/modules/pages/pages.module.js
@@ -0,0 +1,18 @@
+/**
+ * pages Module
+ *
+ * Description
+ */
+(function(window, angular) {
+  'use strict';
+
+  angular.module('pages', [
+    'pages.header',
+    'pages.footer',
+    'pages.home',
+    'pages.error',
+    'pages.profile',
+    'pages.landing',
+  ]);
+
+})(window, window.angular);
