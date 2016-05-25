043d786101ede612bdf88d4140214a49feec517f
rudolfsonjunior <rudolfson.junior@gmail.com>
2016-05-26 00:53:02 +0200

    CHANGED: pipeline.js, added the style folder and files to the grunt task

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -14,7 +14,8 @@
 
 // The CSS files from our bower components
 var cssFilesToInject = [
-  '_css/**/*.css'
+  '_css/**/*.css',
+  'style/**/*.css'
 ];
 
 // The Js files from our bower components
@@ -37,16 +38,6 @@ var jsFilesToInject = [
 
 ];
 
-
-// Client-side HTML templates are injected using the sources below
-// The ordering of these templates shouldn't matter.
-// (uses Grunt-style wildcard/glob/splat expressions)
-//
-// By default, Sails uses JST templates and precompiles them into
-// functions for you.  If you want to use jade, handlebars, dust, etc.,
-// with the linker, no problem-- you'll just want to make sure the precompiled
-// templates get spit out to the same file.  Be sure and check out `tasks/README.md`
-// for information on customizing and installing new tasks.
 var templateFilesToInject = [
   'src/**/*.html',
   'src/*.html'
