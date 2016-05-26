8fc5eb9a9ba40625171f5ebdc71d92f2fa3696f1
rudolfsonjunior <rudolfson.junior@gmail.com>
2016-05-26 14:40:00 +0200

    CHANGED: hasValidToken to JWTService; ADDED: api/controllers/AuthController.js

--- api/policies/hasValidToken.js
+++ api/policies/hasValidToken.js
@@ -1,15 +1,1 @@
-var jwt = require('jsonwebtoken'),
-    secret = 'yourSecretKey';
 
-module.exports={
-
-  issue: function(payload){
-    return jwt.sign(payload, secret, {expiresIn: 10800});
-  },
-
-  verify: function(token, cb){
-    return jwt.verify(token, secret, {}, cb);
-  }
-
-
-};
---
+++ api/services/JWTService.js
@@ -0,0 +1,20 @@
+/**
+ * JWTService
+ *
+ * @description ::
+ */
+
+var jwt = require('jsonwebtoken'),
+  secret = 'yourSecretKey';
+
+module.exports={
+
+  issue: function(payload){
+    return jwt.sign(payload, secret, {expiresIn: 10800});
+  },
+
+  verify: function(token, cb){
+    return jwt.verify(token, secret, {}, cb);
+  }
+
+};
