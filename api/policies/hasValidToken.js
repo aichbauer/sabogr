81674b2fdfd390a0924f72c87e36dcd732c7b39c
rudolfsonjunior <rudolfson.junior@gmail.com>
2016-05-26 14:46:27 +0200

    CHANGED: api/policies/hasValidToken.js, added the function to check whether the token is set in the header as Authorization: Bearer [token], or a request param called token or no authorization was found. Next it verifies if the token is valid or invalid

--- api/policies/hasValidToken.js
+++ api/policies/hasValidToken.js
@@ -1,1 +1,33 @@
+var JWTService = require("../services/JWTService.js");
 
+//usually: "Authorization: Bearer [token]" or "token: [token]"
+
+
+module.exports= function(req, res, next){
+  var token;
+
+  if(req.headers && req.headers.authorization){
+    var parts = req.headers.authorization.split(" ");
+    if(parts.length == 2){
+      var scheme = parts[0],
+        credentials = parts[1];
+
+      if(/^Bearer$/i.test(scheme)){
+        token = credentials;
+      }
+    }else{
+      return res.json(401, {err: "Format for Authorization: Bearer [token]"});
+    }
+  } else if(req.param("token")){
+    token = req.param("token");
+    delete req.query.token;
+  }else{
+    return res.json(401, {err: "No Authorization was found"});
+  }
+
+  JWTService.verify(token, function(err, token){
+    if(err) return res.json(401, {err: "Invalid Token!"});
+    req.token = token;
+    next();
+  });
+};
