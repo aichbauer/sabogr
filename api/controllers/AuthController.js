ea0a76806283c69e6ef413a7a897b5dfad038e27
rudolfsonjunior <rudolfson.junior@gmail.com>
2016-05-26 15:06:07 +0200

    CHANGED: api/controllers/AuthController.js, added register and login, when login or register successful: send token to client

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -6,6 +6,56 @@
  */
 
 module.exports = {
-	
+
+  register: function (req, res) {
+
+    var body = req.allParams();
+
+    if (body.email && body.username && body.password) {
+
+      User.create({username: body.username, email: body.email, password: body.password})
+        .then(function (user) {
+          return res.json(200, {user: user, token: JWTService.issue({id: user.id})});
+        })
+        .fail(function (err) {
+          return res.negotiate(err);
+        });
+
+    } else {
+      return res.json(400, {msg: 'Username, Email, Password are required!'});
+    }
+
+  },
+
+  login: function (req, res) {
+
+    var pw = req.param('password');
+    var username = req.param('username');
+
+    if (pw && username) {
+      User.findOne({username: username})
+        .then(function (user) {
+          if (!user) {
+            return res.json(400, {msg: 'username or password is not correct'});
+          }
+          else {
+            User.comparePassword(pw, user, function (err, match) {
+              if (err) {
+                return res.negotiate(err);
+              }
+              else {
+                if (match) {
+                  return res.json(200, {user: user, token: JWTService.issue({id: user.id})});
+                }
+                else {
+                  return res.json(400, {msg: 'username or password is not correct'});
+                }
+              }
+            })
+          }
+        })
+    }
+  }
+
 };
 
